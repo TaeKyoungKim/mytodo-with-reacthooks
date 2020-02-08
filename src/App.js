3b8b610037967c72c3779c66852db8fe9beafb7b
Jameskim <moduedu@gmail.com>
2020-02-08 21:15:39 +0900

    localhost:8080/todo 연결하여 데이터 뿌리기

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React ,{useState, useEffect }from 'react';
 import {List} from './List'
 
 const App = ()=>{
-  const [todos, setTodo] = useState(['Js Study']);
+  const [todos, setTodo] = useState([]);
   const [newTodo , setNewTodo] = useState()
+  const [loading , setLoading] = useState(false)
 
   const ChangeInputData = (e)=>{
     setNewTodo(e.target.value);
@@ -15,16 +16,30 @@ const App = ()=>{
     setTodo([...todos, newTodo]);
   }
 
+  const fetchInitialData = async ()=>{
+    setLoading(true)
+    var response = await fetch(`http://localhost:8080/todo`)
+    var res = await response.json()
+    await console.log(res)
+    await setTodo(res.data)  
+    setLoading(false)
+
+  }
   useEffect(()=>{
     console.log("새로운 내용이 입력 되었습니다" ,todos)
   },[todos])
+  
+  useEffect(()=>{
+    fetchInitialData()
+  },[])
+
   return (
     <div className="App">
     <h1>ToDo Application </h1>
     <form>
       <input type="text" className="" onChange={ChangeInputData}></input>
       <button className="" onClick={addTodo}>할일추가</button>
-      <List todos={todos}/>
+      <List todos={todos} loading={loading}/>
     </form>
     </div>
   );
--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const List = ({todos}) => {
-    const todoLists = todos.map(todo=>{
-       return <li>{todo}</li>
-    })
+export const List = ({todos ,loading}) => {
+    let todoLists = <h1>TodoList is now Loading....</h1>
+    if(!loading) todoLists = todos.map(todo=><li key={todo._id} >{todo.title}</li>)
+
     return (
         <div>
            <ul>
